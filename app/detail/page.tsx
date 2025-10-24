2b3fe0695f1c2deeee5cd0aff742d8321fdec599
Zumi <akumuis1@gmail.com>
2025-10-24 23:18:26 +0700

    Setup example HomePage and DetailPage

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,37 +1,33 @@
-"use client";
-
-import { Breadcrumb, Layout, Menu, theme } from "antd";
-import { Content, Footer, Header } from "antd/es/layout/layout";
-
-const items = Array.from({ length: 3 }).map((_, index) => ({
-  key: String(index + 1),
-  label: `nav ${index + 1}`,
-}));
+import { Button } from "antd";
+import Card from "antd/es/card/Card";
+import Meta from "antd/es/card/Meta";
+import Link from "next/link";
 
 export default function DetailPage() {
   return (
-    <Layout className="h-[1080px] min-h-dvh">
-      <Header className="sticky top-0 z-1 flex w-full items-center">
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={items}
-          className="min-w-0 grow"
+    <>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            draggable={false}
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <Meta
+          title="Ini adalah halaman detail"
+          description="www.instagram.com"
         />
-      </Header>
+      </Card>
 
-      <Content className="px-12">
-        <Breadcrumb
-          className="py-4"
-          items={[{ title: "Home" }, { title: "List" }, { title: "App" }]}
-        />
-        <div className="min-h-[380px] rounded-lg bg-white p-6">Content</div>
-      </Content>
-
-      <Footer className="text-center">
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
+      <Link href="/">
+        <Button variant="solid" color="volcano">
+          Ke halaman home
+        </Button>
+      </Link>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
-import { Calendar } from "antd";
+import { Breadcrumb, Button, Menu } from "antd";
+import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import Link from "next/link";
 
-export default function Home() {
+const items = Array.from({ length: 3 }).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+export default async function HomePage() {
   return (
-    <main>
-      <Calendar />
-    </main>
+    <Layout className="h-[1080px] min-h-dvh">
+      <Header className="sticky top-0 z-1 flex w-full items-center">
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          className="min-w-0 grow"
+        />
+      </Header>
+
+      <Content className="px-12">
+        <Breadcrumb className="py-4" items={[{ title: "Home" }]} />
+        <div className="min-h-[380px] rounded-lg bg-white p-6">
+          <p>Selamat datang di situs direktori UMKM! 👋</p>
+
+          <Link href="/detail">
+            <Button variant="solid" color="volcano">
+              Ke halaman detail
+            </Button>
+          </Link>
+        </div>
+      </Content>
+
+      <Footer className="text-center">
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
